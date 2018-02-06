cd02fb53554069800c8e31554368b4354595dca8
Santiago <santiago_1016@hotmail.com>
2018-02-06 10:02:54 -0500

    creando primeros componentes

---
+++ app/assets/javascripts/components/app.jsx
@@ -0,0 +1,10 @@
+var App = createReactClass({
+	render: function(){
+		return(
+			<div>
+				<h1>ODOT</h1>
+				<List />
+			</div>		
+		);
+	}
+});
---
+++ app/assets/javascripts/components/list.jsx
@@ -0,0 +1,14 @@
+var List = createReactClass({
+	render: function(){
+	return(
+		<div className = "player">
+			<div className = "player-name">
+				<p className="remove-player">x</p>
+			</div>
+			<div className = "player-score">
+				<p className="remove-player">x</p>
+			</div>	
+		</div>
+		);
+	}
+});
